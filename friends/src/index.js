33c53e69e393c1d510ad2bac01b88200ea04a59f
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-03-06 15:38:11 +0000

    Configure Redux & Async Redux (index.js)

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({ });
+
+const store = createStore(
+  rootReducer,
+  {},
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
