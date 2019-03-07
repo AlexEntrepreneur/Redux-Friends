56d59ed5afa73605134e0b6e3f66ae158043dbb8
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-03-07 16:17:04 +0000

    Install React Router

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import thunk from 'redux-thunk';
 import {
   friendsReducer
@@ -21,7 +22,9 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
